60457281a44b04640708c47e4955215787d41be0
dhpitt <david.h.pitt@gmail.com>
2024-04-23 15:07:47 -0400

    add int --> float test

--- src/configmypy/tests/test_argparse_config.py
+++ src/configmypy/tests/test_argparse_config.py
@@ -3,9 +3,9 @@ from ..bunch import Bunch
 
 
 TEST_CONFIG_DICT = {
-        'default': {'opt': {'optimizer': 'adam', 'lr': 0.1, 'regularizer': True},
+        'default': {'opt': {'optimizer': 'adam', 'lr': 1, 'regularizer': True},
               'data': {'dataset': 'ns', 'batch_size': 12, 'test_resolutions':[16,32]}},
-  'test': {'opt': {'optimizer': 'SGD'}}
+        'test': {'opt': {'optimizer': 'SGD'}}
 }
 
 
@@ -18,20 +18,24 @@ def test_ArgparseConfig(monkeypatch):
     assert config == args
     assert kwargs == {}
     
-    # test ability to infer None and iterables
+    # test ability to infer None, int-->float and iterables
     monkeypatch.setattr("sys.argv", ['test', '--data.batch_size', '24',\
-                                    '--data.test_resolutions', '[8, None]',
-                                     '--opt.regularizer', 'False',
+                                    '--data.test_resolutions', '[8, None]',\
+                                     '--opt.lr', '0.01',\
+                                     '--opt.regularizer', 'False',\
                                      '--config_name', 'test'])
     config = Bunch(TEST_CONFIG_DICT['default'])
     parser = ArgparseConfig(infer_types='fuzzy', config_name=None)
     args, kwargs = parser.read_conf(config)
     config.data.batch_size = 24
+    assert args.opt.lr == 0.01 # ensure no casting to int
     assert args.data.test_resolutions == [8, None]
     assert not args.opt.regularizer #boolean False, not 'False'
 
+
     args.data.test_resolutions = [16,32]
     args.opt.regularizer = True
+    args.opt.lr = 1
     assert config == args
     assert kwargs == Bunch(dict(config_name='test'))
 
